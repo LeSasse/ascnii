7a0211f812355d18a4c2922efbe8360d859152d7
LeSasse <l.sasse@fz-juelich.de>
2023-01-23 12:23:58 +0100

    first working version with some command line options

---
+++ ascnii/args.py
@@ -0,0 +1,102 @@
+"""Provide arg parsing functionality for ascnii package."""
+
+# Authors: Leonard Sasse <l.sasse@fz-juelich.de>
+# License: AGPL
+
+from argparse import ArgumentParser
+from pathlib import Path
+
+
+def parse_args():
+    """Parse ascnii arguments."""
+    background_colors = [
+        "black",
+        "red",
+        "red",
+        "green",
+        "yellow",
+        "blue",
+        "magenta",
+        "cyan",
+        "white",
+    ]
+    parser = ArgumentParser(
+        description=("View NIfTI images in terminal using ASCII characters.")
+    )
+
+    parser.add_argument("nifti", type=Path, help="Path to the NIfTI file.")
+
+    parser.add_argument(
+        "-H",
+        "--header",
+        action="store_true",
+        help="If activated, print out the image header as well.",
+    )
+
+    parser.add_argument(
+        "-C",
+        "--columns",
+        default=120,
+        type=int,
+        help="Number of character columns (in terminal) to use for plotting.",
+    )
+
+    parser.add_argument(
+        "-c",
+        "--colorbar",
+        action="store_true",
+        help="If activated, a colorbar is plotted with the brain images.",
+    )
+
+    parser.add_argument(
+        "-m",
+        "--colormap",
+        default="viridis",
+        type=str,
+        help="Choose a colormap for the image.",
+    )
+
+    parser.add_argument(
+        "-b",
+        "--background",
+        default="black",
+        choices=background_colors,
+        type=str,
+        help="Choose a terminal color for the background.",
+    )
+
+    parser.add_argument(
+        "-t",
+        "--threshold",
+        default=0.1,
+        type=float,
+        help="Threshold the given image below this value.",
+    )
+
+    parser.add_argument(
+        "-cc",
+        "--cut_coords",
+        default=[-10, 0, 0],
+        type=int,
+        nargs=3,
+        help="MNI coordinates at which to plot the image.",
+    )
+
+    parser.add_argument(
+        "-a",
+        "--annotate",
+        action="store_true",
+        help="If activated, positions and left/right annotation are added.",
+    )
+
+    parser.add_argument(
+        "-d",
+        "--draw_cross",
+        action="store_true",
+        help=(
+            "If activated, a cross is drawn on the"
+            " plot to indicate the cut position."
+        ),
+    )
+
+    return parser.parse_args()
--- ascnii/ascnii.py
+++ ascnii/ascnii.py
@@ -3,9 +3,68 @@
 # Authors: Leonard Sasse <l.sasse@fz-juelich.de>
 # License: AGPL
 
+import tempfile
+from pathlib import Path
+
+import ascii_magic
+from nilearn import datasets, image, plotting
+
 from .args import parse_args
 
+
+def get_background_color(color_string):
+    """Get background color object for ascii_magic from string."""
+    colors = {
+        "black": ascii_magic.Back.BLACK,
+        "red": ascii_magic.Back.RED,
+        "green": ascii_magic.Back.GREEN,
+        "yellow": ascii_magic.Back.YELLOW,
+        "blue": ascii_magic.Back.BLUE,
+        "magenta": ascii_magic.Back.MAGENTA,
+        "cyan": ascii_magic.Back.CYAN,
+        "white": ascii_magic.Back.WHITE,
+    }
+
+    return colors[color_string]
+
+
 def main():
-    
+    """Run main program."""
     args = parse_args()
-    print(args.nifti) 
+    img = image.load_img(args.nifti)
+    header = img.header
+    if img.ndim == 4:
+        img = image.mean_img(img)
+
+    mask = datasets.load_mni152_template()
+    mask = image.resample_to_img(mask, img)
+    img = image.math_img("img * mask", img=img, mask=mask)
+
+    with tempfile.TemporaryDirectory() as tmp:
+        tmp = Path(tmp)
+        img_plotted = plotting.plot_img(
+            img,
+            cut_coords=tuple(args.cut_coords),
+            resampling_interpolation="nearest",
+            colorbar=args.colorbar,
+            black_bg=True,
+            threshold=args.threshold,
+            annotate=args.annotate,
+            draw_cross=args.draw_cross,
+            cbar_tick_format="%i",
+            cmap=args.colormap,
+        )
+
+        img_plotted.savefig(tmp / "plot.png")
+        img_ascii = (
+            ascii_magic.from_image_file(
+                tmp / "plot.png",
+                columns=args.columns,
+                back=get_background_color(args.background),
+            )
+            .replace(")", " ")
+            .replace("=", " ")
+        )
+        ascii_magic.to_terminal(img_ascii)
+        if args.header:
+            print(header)
